d58840951dbdca9e0acd43fce731cbac5736b6f2
bussiere <bussiere@gmail.com>
2013-02-28 08:56:05 +0100

    debut

---
+++ PirateBoxMessageBoard/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+#admin.autodiscover()
+admin.autodiscover()
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'views.home', name='home'),
+    # url(r'^Jackpoint/', include('foo.urls')),
+    url(r'^/$', 'message.views.index',name='message-index'),
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
