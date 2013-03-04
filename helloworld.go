c1ca59d422b45c0bf6f03cc8487f2c7d5c7a8a33
bussiere <bussiere@gmail.com>
2013-03-04 15:44:07 +0100

    rajout

--- helloworld.go
+++ helloworld.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"net/http"
+	"strconv"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Bonjour <br><a href='irc://10.0.0.1/42'>Serveur Irc sur port 667 ip : 10.0.0.1 channel principal #42</a><br><a href='sftp://pirate:pirate@10.0.0.1'>Server de Partage de fichier en sftp sur le port 22 avec id : pirate et mdp : pirate </a>")
+func HelloServer(c http.ResponseWriter, req *http.Request) {
+	result := "Bonjour <br><a href='irc://10.0.0.1/42'>Serveur Irc sur port 667 ip : 10.0.0.1 channel principal #42</a><br><a href='sftp://pirate:pirate@10.0.0.1'>Server de Partage de fichier en sftp sur le port 22 avec id : pirate et mdp : pirate </a><br>Done By Bussiere"
+	c.Header().Set("Content-Type", "text/html")
+	c.Header().Set("Content-Length", strconv.Itoa(len(result)))
+	io.WriteString(c, result)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.Handle("/", http.HandlerFunc(HelloServer))
 	http.ListenAndServe(":80", nil)
 }
